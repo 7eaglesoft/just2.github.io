179b936b757adaed77116e74e189a87f9f1e768f
7eaglesoft <74428742+7eaglesoft@users.noreply.github.com>
2020-11-14 17:51:01 +0530

    Create apidashboard.php

---
+++ apidashboard.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$age = array("1"=>"just.my-style.in/e/emvapi.php", "2"=>"just.my-style.in/e/Views.php", "3"=>"just.my-style.in/e/emvapi.php" 
+, "4"=>"just.my-style.in/e/emvapi.php" , "5"=>"just.my-style.in/e/emvapi.php" , "6"=>"just.my-style.in/e/emvapi.php" , 
+"7"=>"just.my-style.in/e/emvapi.php" , "8"=>"just.my-style.in/e/emvapi.php");
+
+//echo json_encode($age);
+echo "hello";
+
+?>
+
+</body>
+</html>
